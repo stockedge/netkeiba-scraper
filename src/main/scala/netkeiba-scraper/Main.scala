d9ce5c46628293c3b8c37d6313fccd20959fda55
polio <eisa1272000@yahoo.co.jp>
2016-01-10 17:54:16 +0900

    Add PayoffDao

--- src/main/scala/netkeiba-scraper/Main.scala
+++ src/main/scala/netkeiba-scraper/Main.scala
@@ -564,6 +564,50 @@ insert or replace into race_result (
   }
   
 }
+
+case class Payoff(
+  race_id: Int,
+  ticket_type: Int,
+  horse_number: String,
+  payoff: Double,
+  popularity: Int
+)
+
+object PayoffDao {
+
+  def createTable()(implicit s: DBSession) = {
+    sql"""
+create table if not exists payoff (
+  race_id      integer not null,
+  ticket_type  integer not null check(ticket_type between 0 and 7),
+  horse_number text    not null,
+  payoff       real    not null check(payoff >= 0),
+  popularity   integer not null check(popularity >= 0),
+  primary key (race_id, ticket_type, horse_number),
+  foreign key (race_id) references race_info (id)
+)
+""".execute.apply
+  }
+
+  def insert(dto: Payoff)(implicit s: DBSession) = {
+    sql"""
+insert or replace into payoff (
+  race_id,
+  ticket_type,
+  horse_number,
+  payoff,
+  popularity
+) values (
+  ${dto.race_id},
+  ${dto.ticket_type},
+  ${dto.horse_number},
+  ${dto.payoff},
+  ${dto.popularity},
+)
+""".update.apply()
+  }
+
+}
  
 object FeatureGenerator {
  
